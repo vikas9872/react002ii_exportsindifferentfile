7ce85d7ba8c3e8cd2d15a3566d501fc9616f9822
vikas9872 <shanabhogvikas648.com>
2024-03-15 22:27:44 +0530

    002ii Exports in the different file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './HeaderFooterWorld';
+import { Main } from './HeaderFooterWorld';
+import { Footer } from './HeaderFooterWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/HeaderFooterWorld.js
@@ -0,0 +1,13 @@
+function Header(){
+    return <h1>This is the Header Part</h1>
+}
+
+// Named export
+export function Main(){
+    return <h1>This is the Main Part</h1>
+}
+export function Footer(){
+    return <h1>This is the Footer Part</h1>
+}
+
+export default Header;
